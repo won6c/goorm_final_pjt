af2063d59c812bd8736b0593e8f94e96c7d893df
won6c <superken1227@naver.com>
2025-02-08 22:00:54 +0900

    edit network, add event

--- func_script/MA_DA/MA_DA.py
+++ func_script/MA_DA/MA_DA.py
@@ -6,8 +6,10 @@ from process.process_list import *
 from reg.reg_capture_test import *
 from vola.script.procdump import *
 from common import stop_event
-import os, time, psutil
 from concurrent.futures import ThreadPoolExecutor, as_completed
+from event.event_security import *
+from event.event_system import *
+import os, time, psutil, copy
 
 def wait_for_termination_or_timeout(pid, timeout=60):
     start_time = time.time()
@@ -26,11 +28,13 @@ def process():
 
     before_capture = capture()
     process_time = time.time()
-    with ThreadPoolExecutor(max_workers=3) as executor: # 함수를 추가할 수록 max_workers의 값을 추가하는 함수의 개수만큼 추가해야 함
+    with ThreadPoolExecutor(max_workers=6) as executor: # 함수를 추가할 수록 max_workers의 값을 추가하는 함수의 개수만큼 추가해야 함
         future_process_list = executor.submit(process_list)
+        future_event_security_list = executor.submit(monitor_security_event_log,"localhost","Security",[4624, 4625, 4672])
+        future_event_system_list = executor.submit(monitor_system_event_log,"localhost","System",[4624, 4625, 4672])
         time.sleep(5)
         pid = spawn_frida_to_process(file)
-
+        future_network = executor.submit(process_network)
         session = resume_frida_with_process(pid)
         if not session:
             print("Process Not Executed or resume failed")
@@ -45,11 +49,17 @@ def process():
         result_process_list = future_process_list.result()
         result_frida = future_frida.result()
         result_dll = future_dll.result()
-    
+        result_event_security = future_event_security_list.result()
+        result_event_system = future_event_system_list.result()
+        result_network = future_network.result()
+
     result_dict = {
         "process_list": result_process_list,
         "process_frida": result_frida,
-        "process_dll": result_dll
+        "process_dll": result_dll,
+        "event_security":result_event_security,
+        "event_system":result_event_system,
+        "network_traffic":result_network,
     }
 
     after_capture = capture()
@@ -59,6 +69,10 @@ def process():
     for func, res in result_dict.items():
         print(f"{func} -> {res}")
     print(f"Execution time : {time.time()-process_time}")
+
+    result_dict_copy = copy.deepcopy(result_dict)
+    with open('mid_result.json', 'w', encoding='utf-8') as f:
+        json.dump(result_dict_copy, f, indent=4, ensure_ascii=False)
 def main():
     process()
 
--- func_script/MA_DA/network/network_packet.py
+++ func_script/MA_DA/network/network_packet.py
@@ -1,219 +1,46 @@
 from scapy.all import *
+from common import stop_event
+import base64
 
-#데이터 유출 및 수집
-def capture_data(interface):
-    """
-    
-    Ether / IP / UDP / DNS Qry b'clientservices.googleapis.com.'
-    Ether / IP / UDP / DNS Qry b'clientservices.googleapis.com.'
-    Ether / IP / UDP / DNS Ans 142.250.196.99
-    Ether / IP / UDP / DNS Ans
-    Ether / IP / TCP 10.0.2.15:49877 > 142.250.196.99:https S
-    Ether / IP / UDP / DNS Qry b'accounts.google.com.'
-    Ether / IP / UDP / DNS Qry b'accounts.google.com.'
-    Ether / IP / UDP / DNS Qry b'www.google.com.'
-    Ether / IP / UDP / DNS Qry b'www.google.com.'
-    Ether / IP / UDP / DNS Ans 74.125.23.84
-    
-    """
-    packets = sniff(iface=interface,count=10)
-    for packet in packets:
-        print(packet)
-        if Raw in packet:
-            print(packet[Raw].load)
+result_network = {}
+result_network["network"]=[]
 
-#capture_data("Ethernet")
-
-#DNS 트래픽 분석
-def dns_analyze(packet):
-
-    """
-    
-    DNS Qry b'clientservices.googleapis.com.'
-    DNS Qry b'clientservices.googleapis.com.'
-    DNS Ans 216.58.220.99
-    DNS Ans
-    DNS Qry b'accounts.google.com.'
-    DNS Qry b'accounts.google.com.'
-    DNS Qry b'www.google.com.'
-    DNS Qry b'www.google.com.'
-    DNS Ans 74.125.203.84
-    DNS Ans
-    DNS Ans 142.250.196.132
-    DNS Ans
-    DNS Qry b'ogads-pa.googleapis.com.'
-    DNS Qry b'ogads-pa.googleapis.com.'
-    DNS Ans 142.251.42.170
-    DNS Ans
-    DNS Qry b'play.google.com.'
-    DNS Qry b'play.google.com.'
-    DNS Qry b'jvvp.tistory.com.'
-    DNS Qry b'jvvp.tistory.com.'
-    DNS Ans b'wildcard-tistory-fz0x1pwf.kgslb.com.'
-    DNS Qry b'google-ohttp-relay-safebrowsing.fastly-edge.com.'
-    DNS Qry b'google-ohttp-relay-safebrowsing.fastly-edge.com.'
-    DNS Ans 146.75.49.91
-    DNS Ans
-    DNS Qry b'play.google.com.'
-    DNS Qry b'play.google.com.'
-    DNS Qry b'jvvp.tistory.com.'
-    DNS Qry b'play.google.com.'
-    DNS Qry b'play.google.com.'
-    DNS Qry b'jvvp.tistory.com.'
-    DNS Ans 172.217.25.174
-    DNS Ans
-    DNS Ans b'wildcard-tistory-fz0x1pwf.kgslb.com.'
-    DNS Qry b'search1.daumcdn.net.'
-    DNS Qry b'search1.daumcdn.net.'
-    DNS Qry b't1.daumcdn.net.'
-    DNS Qry b't1.daumcdn.net.'
-    DNS Ans b'search-xi6mgp35.kgslb.com.'
-    DNS Ans b'search-xi6mgp35.kgslb.com.'
-    DNS Ans b't1-wg2vgaja.kgslb.com.'
-    DNS Ans b't1-wg2vgaja.kgslb.com.'
-    DNS Qry b'googleads.g.doubleclick.net.'
-    DNS Qry b'googleads.g.doubleclick.net.'
-    DNS Ans 172.217.174.98
-    DNS Ans
-    DNS Qry b'pagead2.googlesyndication.com.'
-    DNS Qry b'pagead2.googlesyndication.com.'
-    DNS Ans 172.217.26.226
-    DNS Ans
-    DNS Qry b'webid.ad.daum.net.'
-    DNS Qry b'webid.ad.daum.net.'
-    DNS Ans b'webid-73kbtbvm.kgslb.com.'
-    DNS Ans b'webid-73kbtbvm.kgslb.com.'
-    DNS Qry b'www.googletagmanager.com.'
-    DNS Qry b'www.googletagmanager.com.'
-    DNS Qry b'www.google-analytics.com.'
-    DNS Qry b'www.google-analytics.com.'
-    DNS Ans 142.251.222.40
-    DNS Ans
-    DNS Ans b'www-alv.google-analytics.com.'
-    DNS Ans 142.250.207.14
-    DNS Qry b'content-autofill.googleapis.com.'
-    DNS Qry b'content-autofill.googleapis.com.'
-    DNS Qry b'webid.kakao.com.'
-    DNS Qry b'webid.kakao.com.'
-    DNS Ans 172.217.174.106
-    DNS Ans
-    DNS Ans b'webid-73kbtbvm.kgslb.com.'
-    DNS Ans b'webid-73kbtbvm.kgslb.com.'
-    DNS Qry b'googleads.g.doubleclick.net.'
-    DNS Qry b'googleads.g.doubleclick.net.'
-    DNS Ans 172.217.174.98
-    DNS Ans
-    DNS Qry b'display.ad.daum.net.'
-    DNS Qry b'display.ad.daum.net.'
-    DNS Ans b'display-ad-rgt0me85.kgslb.com.'
-    DNS Ans b'display-ad-rgt0me85.kgslb.com.'    
-    
-    """
-
-    if packet.haslayer(DNS):
-        print(packet[DNS].summary())
-
-#sniff(prn=dns_analyze, filter="udp port 53", store=0)
-
-#비정상적인 프로토콜 및 포트 사용 <- 이상함
-def analyze_protocol_port(packet):
-    if packet.haslayer(TCP) or packet.haslayer(UDP):
-        sport = packet.sport
-        dport = packet.dport
-        if dport not in [80, 443, 53]: #Well Known Port인 포트들을 배열이 추가
-            print(packet.summary)
-
-#sniff(prn=analyze_protocol_port, store = 0)
-
-#패킷 페이로드 분석
-def payload_analyze(packet):
-    if packet.haslayer(Raw):
-        payload = packet[Raw].load
-        #페이로드 분석 로직 추가
-        print(payload)
-
-#sniff(prn=payload_analyze, store = 0)
-
-#연결대상 분석
-def analyze_connection(packet):
-
-    """
-    
-    Source : 10.0.2.15 -> Destination : 20.189.173.7
-    Source : 10.0.2.15 -> Destination : 20.189.173.7
-    Source : 10.0.2.15 -> Destination : 20.189.173.7
-    Source : 20.189.173.7 -> Destination : 10.0.2.15
-    
-    """
-
+def process_packet(packet):
     if packet.haslayer(IP):
         src_ip = packet[IP].src
         dst_ip = packet[IP].dst
-        print(f"Source : {src_ip} -> Destination : {dst_ip}")
-
-#sniff(prn=analyze_connection, store = 0)
-
-#트래픽 패턴 및 행동 분석
-def analyze_traffic_patterns(packet):
-
-    """
+        protocol = packet[IP].proto
+        if protocol==6:
+            protocol="TCP"
+        elif protocol==17:
+            protocol="UDP"
+    else:
+        return
     
-    Ether / IP / TCP 10.0.2.15:49984 > 51.105.71.136:https FA
-    Ether / IP / TCP 51.105.71.136:https > 10.0.2.15:49984 A / Padding
-    Ether / IP / TCP 51.105.71.136:https > 10.0.2.15:49984 FA / Padding
-    Ether / IP / TCP 10.0.2.15:49984 > 51.105.71.136:https A
-    Ether / IP / TCP 10.0.2.15:49985 > 118.214.79.16:http FA
-    Ether / IP / TCP 118.214.79.16:http > 10.0.2.15:49985 A / Padding
-    Ether / IP / TCP 118.214.79.16:http > 10.0.2.15:49985 FA / Padding
-    Ether / IP / TCP 10.0.2.15:49985 > 118.214.79.16:http A
-    Ether / IP / UDP / mDNS Ans b'WinDev2407Eval._dosvc._tcp.local.'
-    Ether / IP / UDP / mDNS Qry b'WinDev2407Eval._dosvc._tcp.local.'    
-    
-    """
+    if packet.haslayer(TCP):
+        src_port = packet[TCP].sport
+        dst_port = packet[TCP].dport
 
-    if packet.haslayer(IP):
-        #트래픽 패턴 및 행동 분석 로직 추가가
-        print(packet.summary())
+    elif packet.haslayer(UDP):
+        src_port = packet[UDP].sport
+        dst_port = packet[UDP].dport
 
-#sniff(prn = analyze_traffic_patterns, store = 0)
-
-#암호화 및 인코딩 기법 분석
-def analyze_encryption(packet):
-    if packet.haslayer(Raw):
-        #암호화 및 인코딩 분석 로직 추가가
+    if packet.haslayer(Raw) and src_port==80:
         payload = packet[Raw].load
-        print(payload)
-
-#sniff(prn = analyze_encryption, store = 0)
-
-#상호작용하는 내부 호스트 식별
-def identify_internal_hosts(packet):
-
-    """
+        payload_len = len(payload)
+    elif packet.haslayer(Raw):
+        payload = packet[Raw].load
+        payload_len = len(payload)
     
-    Internal Host : 142.250.198.14 -> 10.0.2.15
-    Internal Host : 10.0.2.15 -> 142.250.196.106
-    Internal Host : 142.250.196.106 -> 10.0.2.15
-    Internal Host : 142.250.196.106 -> 10.0.2.15
-    Internal Host : 10.0.2.15 -> 142.250.196.106
-    Internal Host : 10.0.2.15 -> 224.0.0.251
-    Internal Host : 10.0.2.15 -> 224.0.0.251
-    Internal Host : 10.0.2.15 -> 224.0.0.251
+    if packet.haslayer(Raw):#실제 전송시에는 base64.b64encode(payload).decode('ascii') -> payload
+        network_dict = {"protocol":protocol,"src":src_ip,"sport":src_port,"dst":dst_ip,"dport":dst_port,"payload_len":payload_len,"payload":base64.b64encode(payload).decode('ascii')}
+    else:
+        network_dict = {"protocol":protocol,"src":src_ip,"sport":src_port,"dst":dst_ip,"dport":dst_port,"payload_len":"","payload":""}
+
+    result_network["network"].append(network_dict)
     
-    """
 
-    if packet.haslayer(IP):
-        src_ip = packet[IP].src
-        dst_ip = packet[IP].dst
-        #내부 호스트 식별 로직 추가
-        print(f"Internal Host : {src_ip} -> {dst_ip}")
-
-#sniff(prn = identify_internal_hosts, store = 0)
-
-#로그 및 메타데이터 분석
-def analuze_logs(packet):
-    if packet.haslayer(IP):
-        #로그 및 메타데이터 분석 로직 추가
-        print(packet.summary())
-
-#sniff(prn=analuze_logs, store = 0)
+def process_network():
+    packets = sniff(prn = process_packet, store = 1, stop_filter=lambda pkt: stop_event.is_set())
+    wrpcap("captured_packets.pcap", packets)
+    return result_network
