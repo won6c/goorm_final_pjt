9d986a042529c02236c52457f310d4799412242a
won6c <superken1227@naver.com>
2025-02-28 14:52:25 +0900

    edit memory

--- src/dynamic_analysis/memory/memory_dump.py
+++ src/dynamic_analysis/memory/memory_dump.py
@@ -62,7 +62,7 @@ def get_process_pid(process_name):
 def run_procdump_and_analyze(pid):
     chrome_pid = pid
     if chrome_pid:
-        dump_command = [PROCDUMP_PATH, "-ma", str(chrome_pid), DUMP_FILE]
+        dump_command = [PROCDUMP_PATH, "-accepteula", "-ma", str(chrome_pid), DUMP_FILE]
         
         try:
             subprocess.run(dump_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
